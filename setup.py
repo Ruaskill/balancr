ba49e47e2ba4fff91ab2f3d7926842403c28963f
Ruaskill <cdeffishmore@gmail.com>
2025-01-06 04:03:42 +0000

    Initial setup. TechniqueRegistry class also

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="imbalance-framework",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "numpy>=1.21.0",
+        "pandas>=1.3.0",
+        "scikit-learn>=1.0.0",
+        "matplotlib>=3.4.0",
+        "seaborn>=0.11.0",
+        "imbalanced-learn>=0.8.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0.0",
+            "black>=21.0.0",
+            "flake8>=3.9.0",
+        ],
+    },
+    python_requires=">=3.8",
+    author="Conor Doherty",
+    author_email="ruaskillz1@gmail.com",
+    description="A framework for analysing and comparing balancing data techniques",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
