1581128fa24ff11d3712f354f2b02caf88df07c7
Ruaskill <cdeffishmore@gmail.com>
2025-04-23 03:01:43 +0100

    Adding learning curves against imbalanced dataset feature

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name="balancr",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
